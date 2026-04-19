4ab196e416d8a182bc089f443bf598f4b4fa9ae9
Pablo Nieto Arias <pablo.nieto.arias@gmail.com>
2026-04-19 22:35:56 +0200

    Ajustada la duración de sesión

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
-app.permanent_session_lifetime = 99999999
+app.permanent_session_lifetime = timedelta(minutes=30)
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
