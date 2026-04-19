9303334765595fbf6c1e8171788defca7fb59175
Pablo Nieto Arias <pablo.nieto.arias@gmail.com>
2026-04-19 22:40:52 +0200

    Sustituida clave hardcodeada por clave generada aleatoria.

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 from datetime import timedelta
+import os
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
+app.secret_key = os.environ.get('FLASK_SECRET_KEY', os.urandom(32).hex())
 app.permanent_session_lifetime = timedelta(minutes=30)
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SECURE'] = True
